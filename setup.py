a2c3db95ddadf5033001cc9888590b80faeb82cb
denso <jianyuchen@berkeley.edu>
2020-03-20 17:49:03 -0700

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='interp_e2e_driving',
       version='0.1',
       description='Interpretable End-to-end Autonomous Driving',
-      url='https://github.com/cjy1992/interp-e2e-driving.git',
+      url='https://github.com/cjy1992/interp-e2e-driving',
       author='Jianyu Chen',
       author_email='jianyuchen@berkeley.edu',
       license='MIT',
